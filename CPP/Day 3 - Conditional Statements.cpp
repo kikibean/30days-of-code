b96e9763ece95dc0607f5ff96083330509e15ca0
kikibean <kouki_fiona@hotmail.com>
2017-08-21 16:33:51 -0400

    Create Day 3 - Conditional Statements.cpp

---
+++ CPP/Day 3 - Conditional Statements.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+int N; 
+
+scanf("%d",&N);
+
+if( N%2 == 1 || (N%2==0 && N > 5 && N < 21))
+
+    printf("Weird");
+
+else
+
+    printf("Not Weird");
+
+return 0;
+}
+
+
