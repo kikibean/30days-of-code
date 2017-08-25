c86e4a475f189db8d35f64de6487e73b4b77536a
kikibean <kouki_fiona@hotmail.com>
2017-08-25 12:16:41 -0400

    Create Day 7 - Arrays.py

---
+++ Python/Day 7 - Arrays.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+
+print(' '.join([str(arr[-i]) for i in range(1,n+1)]))
+
